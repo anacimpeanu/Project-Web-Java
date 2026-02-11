6862d62c30bee217c64c8fd976952084df771cea
anacimpeanu <115561036+anacimpeanu@users.noreply.github.com>
2026-02-11 13:14:41 +0000

    Add Exception

---
+++ Project/src/main/java/com/kyra/kyra_cosmetics/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.kyra.cosmetics.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
